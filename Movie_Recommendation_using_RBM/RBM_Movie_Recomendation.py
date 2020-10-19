61f1da723444436b25474de4e8c1ea349c1312f5
varunb5 <varunb5@kpit.com>
2020-10-19 21:36:45 +0530

    Second RBM Commit

--- Movie_Recommendation_using_RBM/RBM_Movie_Recomendation.py
+++ Movie_Recommendation_using_RBM/RBM_Movie_Recomendation.py
@@ -73,9 +73,59 @@ class RBM():
         self.W = torch.randn(nv,nh) #Weights intilaized in torch tensor randomly 
         self.a = torch.randn(1,nh) #Bias for hidden nodes 
         self.b = torch.randn(1,nv) #Bias for visible nodes
-#Fucntion to sample probablites ph given v
-#Gibbs sampling     
-    def sample_h(self)        
+        
+#Fucntion to sample probablites ph given v     
+    def sample_h(self,x): 
+        #Compute the probability of h given v, i.e. probabiltiy of the hidden neuron given the value of visible neuron
+        wx = torch.mm(x,self.W.t())
+        activation = wx + self.a.expand_as(wx)
+        p_h_given_v = torch.sigmoid(activation)
+        return p_h_given_v, torch.bernoulli(p_h_given_v)
 
+#Probabilty of the visible nodes
+    def sample_v(self,y): 
+        #Compute the probability of v given h, i.e. probabiltiy of the visible neuron given the value of hidden neuron
+        wy = torch.mm(y,self.W)
+        activation = wy + self.a.expand_as(wy)
+        p_v_given_h = torch.sigmoid(activation)
+        return p_v_given_h, torch.bernoulli(p_v_given_h)
+
+#Contrasted Divergence used to approximate likelyhood gradient
+#Optimize weights to minimze energy
+    def train(self,v0,vk,ph0,phk):
+        #Update Tensor of weights
+        self.W += torch.mm(v0.t(),ph0) - torch(vk.t(),phk)
+        self.b += torch.sum({v0 - vk},0)        
+        self.a += torch.sum({ph0 - phk},0) 
+
+nv = len(training_set[0])       
+nh = 100
+batch_size = 100 
+
+#Create RBM Objects
+rbm = RBM(nv,nh)        
+
+#training the RBM 
+#Number epochs 
+nb_epoch = 10
+for epoch in range(1,nb_epoch+1):
+    train_loss = 0  
+    s = 0.
+    for id_users in range(0,tot_users - batch_size, batch_size):
+        vk = training_set[id_users:id_users+batch_size]
+        v0 = training_set[id_users:id_users+batch_size]
+        ph0,_ = rbm.sample_h(v0)
+        for k in range(10):
+            _,hk =  rbm.sample_h(vk)
+            _,vk = rbm.sample_v(hk)
+            vk[v0<0] = v0[v0<0]
+        phk,_ = rbm.sample_h(vk)
+        rbm.train(v0,vk,ph0,phk)
+        train_loss += torch.mean(torch.abs(v0[v0>=0] - vk[v0>=0]))
+        s += 1.
+    print("Epoch:"+str(epoch)+"Loss: "+str(train_loss/s))   
+    
+            
+        
 
   
